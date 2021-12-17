3ceebc723e8c759fb75f77706d1c2ea5507df3c6
Brazzalotto julien <julien.brazzalotto@gmail.com>
2021-12-17 22:17:37 +0100

    routes commentaires

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const helmet = require("helmet");
 
 const userRoutes = require("./routes/user-routes");
 const postRoutes = require("./routes/post-routes");
-
+const commentRoutes = require("./routes/comment-routes");
 
 
 const connect = async function () {
@@ -36,6 +36,7 @@ app.use(express.json());
 
 app.use("/images", express.static(path.join(__dirname, 'images')));
 app.use("/api/auth", userRoutes);
-app.use('/api/posts', postRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 module.exports = app;
